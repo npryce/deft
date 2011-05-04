78b17fdb3a3c078274aa2dd9c9ef0f9544880357
Nat Pryce <github@natpryce.com>
2011-05-04 03:23:55 +0100

    made tests more reliable

--- src/deft_tests.py
+++ src/deft_tests.py
@@ -1,21 +1,22 @@
 
 import inspect
 import os
+import shutil
 from deft import Ordering
-
-def ensure_dir_exists(dirpath):
-    if not os.path.exists(dirpath):
-        os.makedirs(dirpath)
 
 
 class Ordering_Test:
+    @classmethod
+    def setup_class(c):
+        shutil.rmtree("output/testing/Ordering_Test")
+    
     def test_ordering_is_created_empty(self):
         o = new_ordering()
         
         assert o.is_empty()
 
 
-    def test_can_add_elements(self):
+    def test_can_add_and_look_up_elements(self):
         o = new_ordering()
         
         o.add(10)
@@ -32,22 +33,25 @@ class Ordering_Test:
     
     def test_changes_are_persisted_to_mapped_file(self):
         o = new_ordering()
-        o.add(10)
-        o.add(20)
-        o.add(30)
-        o.add(40)
+        o.add(101)
+        o.add(100)
+        o.add(99)
         
         o2 = new_ordering()
         assert not o2.is_empty()
-        assert o2[0] == 10
-        assert o2[1] == 20
-        assert o2[2] == 30
-        assert o2[3] == 40
+        assert o2[0] == 101
+        assert o2[1] == 100
+        assert o2[2] == 99
     
-    
+
 def new_ordering():
     test_name = inspect.stack()[1][3]
-    path = "output/testing/ordering_for_" + test_name
+    path = "output/testing/Ordering_Test/" + test_name
     ensure_dir_exists(os.path.dirname(path))
     return Ordering(path)
+
+def ensure_dir_exists(dirpath):
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+
 
