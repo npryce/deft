cc13c8f2ece21d26c5ef611ba5b1755598dd7eef
Nat Pryce <github@natpryce.com>
2011-06-23 19:13:54 +0100

    names of common feature properties

---
+++ src/deft/stdproperties.py
@@ -0,0 +1,21 @@
+"""
+The names of common feature properties
+"""
+
+# Category of the issue/feature. E.g.: new-feature, enhancement, bug, security-error
+Category = "category"
+
+# Category of the issue/feature. E.g.: new-feature, enhancement, bug, security-error
+Component = "component"
+
+# The hardware platform(s) that the feature relates to
+HardwarePlatform = "platform"
+
+# The operating system(s) that the feature relates to
+OperatingSystem = "os"
+
+# The URL of the branch on which the feature is being developed
+FeatureBranch = "branch"
+
+# The milestone in which the feature will be, or was, released.
+Milestone = "milestone"
