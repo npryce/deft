d51512d3a618c01005d9ff83720c734db7b85e9f
Nat Pryce <github@natpryce.com>
2011-06-23 19:14:18 +0100

    indexing for new merge-friendly storage forat

--- src/deft/indexing.py
+++ src/deft/indexing.py
@@ -1,7 +1,7 @@
 
 import os
 import mmap
-
+from itertools import count
 
 class Bucket:
     def __init__(self, unsorted_features):
@@ -48,3 +48,58 @@ class Bucket:
     
     def __repr__(self):
         return self.__class__.__name__ + "(" + repr([f for f in self.features]) + ")"
+
+
+class PriorityIndex:
+    def __init__(self, feature_names_in_priority_order):
+        self._features_by_priority = list(feature_names_in_priority_order)
+        self._priorities_by_feature = None
+    
+    def __len__(self):
+        return len(self._features_by_priority)
+    
+    def __iter__(self):
+        return iter(self._features_by_priority)
+    
+    def feature_with_priority(self, n):
+        return self._features_by_priority[n-1]
+    
+    def priority_of_feature(self, feature_name):
+        if self._priorities_by_feature is None:
+            self._priorities_by_feature = dict(zip(self._features_by_priority, count(1)))
+        
+        return self._priorities_by_feature[feature_name]
+    
+    def append(self, new_feature_name):
+        self._features_by_priority.append(new_feature_name)
+        if self._priorities_by_feature is not None:
+            self._priorities_by_feature[new_feature_name] = len(self._features_by_priority)
+    
+    def insert(self, new_feature_name, priority):
+        self._features_by_priority.insert(max(0, priority-1), new_feature_name)
+        self._priorities_changed()
+    
+    def remove(self, feature_name):
+        del self._features_by_priority[self._index_of(feature_name)]
+        self._priorities_changed()
+    
+    def change_priority(self, feature_name, new_priority):
+        self.remove(feature_name)
+        self.insert(feature_name, new_priority)
+    
+    def _priorities_changed(self):
+        self._priorities_by_feature = None
+    
+    def _index_of(self, feature_name):
+        if self._priorities_by_feature is not None:
+            return self._priorities_by_feature[feature_name]-1
+        else:
+            return self._features_by_priority.index(feature_name)
+        
+    def __str__(self):
+        return repr(self)
+    
+    def __repr__(self):
+        return self.__class__.__name__ + "([" + ", ".join(map(repr, self._features_by_priority)) + "])"
+
+
--- src/deft/indexing_tests.py
+++ src/deft/indexing_tests.py
@@ -1,11 +1,172 @@
 
+from itertools import count
 import inspect
 import os
 import shutil
-from deft.indexing import Bucket
+from deft.indexing import Bucket, PriorityIndex
 from deft.tracker import Feature
 from deft.fake_tracker import make_features, fake_feature
 from hamcrest import *
+from nose.tools import raises
+
+
+    
+def assert_feature_priorities(index, *feature_names):
+    for (name, priority) in zip(feature_names, count(1)):
+        assert_that(index.priority_of_feature(name), equal_to(priority),
+                    "priority of " + name + " should be " + str(priority))
+        assert_that(index.feature_with_priority(priority), equal_to(name),
+                    "feature with priority " + str(priority) + " should be " + name)
+    
+    assert_that(list(index), equal_to(list(feature_names)))
+
+
+class PriorityIndex_Test:
+    def test_indexes_features_by_priority(self):
+        index = PriorityIndex(["alice", "bob", "carol", "dave", "eve"])
+        
+        assert_that(index.feature_with_priority(1), equal_to("alice"))
+        assert_that(index.feature_with_priority(2), equal_to("bob"))
+        assert_that(index.feature_with_priority(3), equal_to("carol"))
+        assert_that(index.feature_with_priority(4), equal_to("dave"))
+        assert_that(index.feature_with_priority(5), equal_to("eve"))
+    
+    def test_indexes_priorities_by_feature(self):
+        index = PriorityIndex(["alice", "bob", "carol", "dave", "eve"])
+        
+        assert_that(index.priority_of_feature("alice"), equal_to(1))
+        assert_that(index.priority_of_feature("bob"), equal_to(2))
+        assert_that(index.priority_of_feature("carol"), equal_to(3))
+        assert_that(index.priority_of_feature("dave"), equal_to(4))
+        assert_that(index.priority_of_feature("eve"), equal_to(5))
+    
+    def test_can_iterate_over_feature_names_in_priority_order(self):
+        index = PriorityIndex(["alice", "bob", "carol", "dave", "eve"])
+
+        assert_that(list(index), equal_to(["alice", "bob", "carol", "dave", "eve"]))
+    
+    def test_can_append_features(self):
+        index = PriorityIndex(["alice", "bob"])
+        
+        index.append("carol")
+        assert_feature_priorities(index, "alice", "bob", "carol")
+        
+        index.append("dave")
+        assert_feature_priorities(index, "alice", "bob", "carol", "dave")
+        
+    def test_can_remove_first_feature(self):
+        index = PriorityIndex(["alice", "bob", "carol", "dave"])
+        index.remove("alice")
+        
+        assert_feature_priorities(index, "bob", "carol", "dave")
+        
+    def test_can_remove_middle_feature(self):
+        index = PriorityIndex(["alice", "bob", "carol", "dave"])
+        index.remove("bob")
+        
+        assert_feature_priorities(index, "alice", "carol", "dave")
+        
+    def test_can_remove_last_feature(self):
+        index = PriorityIndex(["alice", "bob", "carol", "dave"])
+        index.remove("dave")
+        
+        assert_feature_priorities(index, "alice", "bob", "carol")
+
+    def test_can_append_after_removing(self):
+        index = PriorityIndex(["alice", "bob", "carol"])
+        
+        index.remove("bob")
+        index.append("dave")
+        
+        assert_feature_priorities(index, "alice", "carol", "dave")
+        
+    def test_can_insert_features_at_front(self):
+        index = PriorityIndex(["alice", "bob", "carol"])
+        
+        index.insert("eve", 1)
+        
+        assert_feature_priorities(index, "eve", "alice", "bob", "carol")
+        
+    def test_can_insert_features_in_middle(self):
+        index = PriorityIndex(["alice", "bob", "carol"])
+        
+        index.insert("eve", 3)
+        
+        assert_feature_priorities(index, "alice", "bob", "eve", "carol")
+        
+    def test_can_insert_features_in_middle(self):
+        index = PriorityIndex(["alice", "bob", "carol"])
+        
+        index.insert("eve", 4)
+        
+        assert_feature_priorities(index, "alice", "bob", "carol", "eve")
+        
+    def test_limits_priorities_to_priority_one_on_insert(self):
+        index = PriorityIndex(["alice", "bob", "carol"])
+        
+        index.insert("eve", 0)
+        
+        assert_feature_priorities(index, "eve", "alice", "bob", "carol")
+
+    def test_limits_priorities_to_lowest_priority_on_insert(self):
+        index = PriorityIndex(["alice", "bob", "carol"])
+        
+        index.insert("eve", 5)
+        
+        assert_feature_priorities(index, "alice", "bob", "carol", "eve")
+    
+    def test_can_move_feature_from_bottom_to_top(self):
+        index = PriorityIndex(["alice", "bob", "carol", "dave"])
+        
+        index.change_priority("dave", 1)
+        
+        assert_feature_priorities(index, "dave", "alice", "bob", "carol")
+
+    def test_can_move_feature_from_top_to_bottom(self):
+        index = PriorityIndex(["alice", "bob", "carol", "dave"])
+        
+        index.change_priority("alice", 4)
+        
+        assert_feature_priorities(index, "bob", "carol", "dave", "alice")
+    
+    def test_can_move_feature_from_middle_to_top(self):
+        index = PriorityIndex(["alice", "bob", "carol", "dave"])
+        
+        index.change_priority("bob", 1)
+        
+        assert_feature_priorities(index, "bob", "alice", "carol", "dave")
+    
+    def test_can_move_feature_from_middle_to_bottom(self):
+        index = PriorityIndex(["alice", "bob", "carol", "dave"])
+        
+        index.change_priority("bob", 4)
+        
+        assert_feature_priorities(index, "alice", "carol", "dave", "bob")
+        
+    def test_can_move_feature_up_in_the_middle(self):
+        index = PriorityIndex(["alice", "bob", "carol", "dave"])
+        
+        index.change_priority("carol", 2)
+        
+        assert_feature_priorities(index, "alice", "carol", "bob", "dave")
+        
+    def test_can_move_feature_down_in_the_middle(self):
+        index = PriorityIndex(["alice", "bob", "carol", "dave"])
+        
+        index.change_priority("bob", 3)
+        
+        assert_feature_priorities(index, "alice", "carol", "bob", "dave")
+        
+    def test_moving_feature_to_same_priority_has_no_effect(self):
+        index = PriorityIndex(["alice", "bob", "carol", "dave"])
+        
+        index.change_priority("bob", 2)
+        
+        assert_feature_priorities(index, "alice", "bob", "carol", "dave")
+    
+    def test_has_useful_repr(self):
+        assert_that(repr(PriorityIndex(['alice', 'bob', 'carol'])), 
+                    equal_to("PriorityIndex(['alice', 'bob', 'carol'])"))
 
 
 class Bucket_Test:
