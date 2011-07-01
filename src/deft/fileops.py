5e1890ce9e4e6fe45200f2891b7e52b3729f1038
Nat Pryce <github@natpryce.com>
2011-07-01 18:29:50 +0100

    purging dead code

--- src/deft/fileops.py
+++ src/deft/fileops.py
@@ -4,26 +4,6 @@ import shutil
 import yaml
 
 
-def save_yaml(path, obj):
-    with open(path, "w") as output:
-        yaml.dump(obj, output, default_flow_style=False)
-
-
-def load_yaml(path):
-    with open(path, "r") as input:
-        return yaml.safe_load(input)
-
-
-def save_text(path, text):
-    with open(path, "w") as output:
-        output.write(text)
-
-
-def load_text(path):
-    with open(path, "r") as input:
-        return input.read()
-
-
 def ensure_dir_exists(dirpath):
     if not os.path.exists(dirpath):
         os.makedirs(dirpath)
